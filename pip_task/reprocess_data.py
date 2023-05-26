3b8b8f14f6fa94424a1c1a7095f56666fc327abc
Vincentx15 <vincent.mallet96@gmail.com>
2023-05-26 15:35:44 +0200

    Add the loading class for the new PIP loading

--- pip_task/reprocess_data.py
+++ pip_task/reprocess_data.py
@@ -226,7 +226,111 @@ def reprocess_data(data_dir):
     df.to_csv(dump_csv)
 
 
+class NewPIP(torch.utils.data.Dataset):
+    def __init__(self, data_dir, neg_to_pos_ratio=1, max_pos_regions_per_ensemble=5,
+                 geometry_path='../data/processed_data/geometry/',
+                 operator_path='../data/processed_data/operator/',
+                 recompute=False):
+        self.neg_to_pos_ratio = neg_to_pos_ratio
+        self.max_pos_regions_per_ensemble = max_pos_regions_per_ensemble
+        self.recompute = recompute
+        csv_path = os.path.join(data_dir, 'all_systems.csv')
+        self.dir_path = os.path.join(data_dir, 'systems')
+        self.df = pd.read_csv(csv_path)
+        self.geometry_path = geometry_path
+        self.operator_path = operator_path
 
+    def get_geometry_dir(self, name):
+        return naming_utils.name_to_dir(name, dir_path=self.geometry_path)
+
+    def get_operator_dir(self, name):
+        return naming_utils.name_to_dir(name, dir_path=self.operator_path)
+
+    def _num_to_use(self, num_pos, num_neg):
+        """
+        Depending on the number of pos and neg of the system, we might want to use
+            different amounts of positive or negative coordinates.
+
+        :param num_pos:
+        :param num_neg:
+        :return:
+        """
+
+        if self.neg_to_pos_ratio == -1:
+            num_pos_to_use, num_neg_to_use = num_pos, num_neg
+        else:
+            num_pos_to_use = min(num_pos, num_neg / self.neg_to_pos_ratio)
+            if self.max_pos_regions_per_ensemble != -1:
+                num_pos_to_use = min(num_pos_to_use, self.max_pos_regions_per_ensemble)
+            num_neg_to_use = num_pos_to_use * self.neg_to_pos_ratio
+        num_pos_to_use = int(math.ceil(num_pos_to_use))
+        num_neg_to_use = int(math.ceil(num_neg_to_use))
+        return num_pos_to_use, num_neg_to_use
+
+    def __len__(self):
+        return len(self.df)
+
+    def get_item(self, index):
+        row = self.df.iloc[index][["system", "name1", "name2"]]
+        dirname, name1, name2 = row.values
+        dirpath = naming_utils.name_to_dir(dirname, dir_path=self.dir_path)
+        dump_path = os.path.join(dirpath, dirname)
+        dump_struct_1 = os.path.join(dump_path, "struct_1.csv")
+        dump_struct_2 = os.path.join(dump_path, "struct_2.csv")
+        dump_pairs = os.path.join(dump_path, "pairs.csv")
+
+        struct_1 = pd.read_csv(dump_struct_1, index_col=0)
+        struct_2 = pd.read_csv(dump_struct_2, index_col=0)
+        pos_pairs_res = pd.read_csv(dump_pairs, index_col=0)
+
+        mapping_1 = {resindex: i for i, resindex in enumerate(struct_1.residue.values)}
+        mapping_2 = {resindex: i for i, resindex in enumerate(struct_2.residue.values)}
+
+        pos_as_array_1 = np.array([mapping_1[resi] for resi in pos_pairs_res['residue0']])
+        pos_as_array_2 = np.array([mapping_2[resi] for resi in pos_pairs_res['residue1']])
+
+        dense = np.zeros((len(struct_1), len(struct_2)))
+        dense[pos_as_array_1, pos_as_array_2] = 1
+        negs_1, negs_2 = np.where(dense == 0)
+
+        pos_array = np.stack((pos_as_array_1, pos_as_array_2))
+        neg_array = np.stack((negs_1, negs_2))
+        num_pos = pos_as_array_1.shape[0]
+        num_neg = negs_1.shape[0]
+        num_pos_to_use, num_neg_to_use = self._num_to_use(num_pos, num_neg)
+        pos_array_idx = np.random.choice(pos_array.shape[1], size=num_pos_to_use)
+        neg_array_idx = np.random.choice(neg_array.shape[1], size=num_neg_to_use)
+        pos_array_sampled = pos_array[:, pos_array_idx]
+        neg_array_sampled = neg_array[:, neg_array_idx]
+
+        # Get all coords, extract the right ones and stack them into (n_pairs, 2, 3)
+        coords_1 = struct_1[['x', 'y', 'z', ]].values
+        coords_2 = struct_2[['x', 'y', 'z', ]].values
+        pos_1, pos_2 = coords_1[pos_array_sampled[0]], coords_2[pos_array_sampled[1]]
+        pos_stack = np.transpose(np.stack((pos_1, pos_2)), axes=(1, 0, 2))
+        neg_1, neg_2 = coords_1[neg_array_sampled[0]], coords_2[neg_array_sampled[1]]
+        neg_stack = np.transpose(np.stack((neg_1, neg_2)), axes=(1, 0, 2))
+        geom_feats_0 = main.get_diffnetfiles(name=name1,
+                                             df=struct_1,
+                                             dump_surf_dir=self.get_geometry_dir(name1),
+                                             dump_operator_dir=self.get_operator_dir(name1),
+                                             recompute=self.recompute)
+        geom_feats_1 = main.get_diffnetfiles(name=name2,
+                                             df=struct_2,
+                                             dump_surf_dir=self.get_geometry_dir(name2),
+                                             dump_operator_dir=self.get_operator_dir(name2),
+                                             recompute=self.recompute)
+
+        if geom_feats_0 is None or geom_feats_1 is None:
+            return None, None, None, None, None, None
+        return name1, name2, pos_stack, neg_stack, geom_feats_0, geom_feats_1
+
+    def __getitem__(self, index):
+        try:
+            res = self.get_item(index)
+            return res
+        except IndentationError:
+            return None
 
 
 if __name__ == '__main__':
