598934545d997ddd18702d9aead1a87df4ef6a57
pvnieo <sohaib.attaiki@gmail.com>
2023-08-10 14:47:57 +0200

    remove unused

--- feat_extractors/__init__.py
+++
@@ -1,6 +0,0 @@
-from .diffusion_net.layers import DiffusionNet
-
-
-__all__ = [
-    "DiffusionNet",
-]
--- feat_extractors/diffusion_net/__init__.py
+++
@@ -1,8 +0,0 @@
-import base_nets.geometry
-import base_nets.layers
-import base_nets.utils
-
-
-__all__ = [
-    "diffusion_net",
-]
